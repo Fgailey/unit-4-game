6280f82f199145d7921d4048da73883b62a7ced1
Foster Gailey <foster@gaileyhome.com>
2019-09-12 19:15:20 -0400

    started adding an object  for fighter

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,55 +1,100 @@
 //variables
 
 //this will be the fighter for the course of the game
-var fighterSelected
-var enemySelect = ["./assets/images/Shelob.jpg", "./assets/images/Witch_King.png", "./assets/images/Lurtz.jpg", "./assets/images/balrog.jpg"];
-var enemyStats = [80, 90, 70, 100]
-var defenderSelection = [];
+var fighterSelected = {
+    charHealth: Number,
+    charAttack: Number,
+};
+var defenderSelection = {
+    defHealth: Number,
+    defAttack: Number,
+};
 
+
+var enemyStats = [80, 90, 70, 100]
+var fighterPicked = false;
+var defenderPicked = false;
 //this will start once the character has been chosen and display the enemy's
-function enemyCreation(){
-        if ($(".enemies").text() === ""){
-            for (var i = 0; i < 4; i++){
-                var newEnemy = $("<img>");
-                newEnemy.addClass("enemyFighter");
-                newEnemy.attr("src", enemySelect[i]);
-                newEnemy.attr("value", enemyStats[i]);
-                $(".enemies").append(newEnemy);
-            }
-        }
-}
+//this doesnt work cause I cant click on one of these
+
+
+
 
 $(document).ready(function() {
 
+    $(".attack").on("click", function(){
+         
+    });
 
     //this creates a copy of the fighter in the fighting section with the value(hp) and fighting class.
     $(".fighters").on("click", function() {
+        if (fighterPicked === false){
+            var fighter = $("<img>");
+            fighter.addClass("chosenFighter");
+            fighter.attr("src", $(this).attr('src'));
+            fighter.attr("value", $(this).attr('value'))
+            $("#fighting").append(fighter);
+            $(this).fadeOut();
 
-        var fighter = $("<img>");
-        fighter.addClass("chosenFighter");
-        fighter.attr("src", $(this).attr('src'));
-        fighter.attr("value", $(this).attr('value'))
-        $("#fighting").append(fighter);
-
+            //need to make an object for the character chosen
+            fighterSelected.charHealth = $(this).attr("data-health");
+            fighterSelected.charAttack = $(this).attr("data-attack");
+            
+            console.log(fighterSelected.charHealth);
+            console.log(fighterSelected.charAttack);
+            
+            //makes it so new fighter cant be picked until game over or reset
+            fighterPicked = true;
+        }
         //this will call on the enemy creation
-        enemyCreation();
+        
         
     });
-
-    $(".enemyFighter").on("click", function(){
-        var defender = $("<img>");
-        defender.addClass("defendingEnemy");
-        defender.attr("src", $(this).attr('src'));
-        defender.attr("value", $(this).attr('value'));
-        $("#defender").append(defender);
+    //this picks the defending enemy
+    $(".enemyFighter").on("click", function(){        
+        //give a red glowing background in the class
+        if (defenderPicked === false) {
+            console.log("working");
+            var defender = $("<img>");
+            defender.addClass("defendingEnemy");
+            defender.attr("src", $(this).attr('src'));
+            defender.attr("value", $(this).attr('value'));
+            $("#defender").append(defender);
+            $(this).fadeOut();
+            defenderPicked = true;
+        }
     });
 
 
     $("#reset").on("click", function() {
         console.log("reset");
         $("#fighting").empty();
-        $(".enemies").empty();
+        $("#defender").empty();
+        $(".fighters").fadeIn();
+        $(".enemyFighter").fadeIn();
 
+        defenderPicked = false;
+        fighterPicked = false;
     });
     
 });
+
+
+//var enemySelect = ["./assets/images/Shelob.jpg", "./assets/images/Witch_King.png", "./assets/images/Lurtz.jpg", "./assets/images/balrog.jpg"];
+
+
+// function enemyCreation(){
+//         if ($(".enemies").text() === ""){
+//             for (var i = 0; i < 4; i++){
+
+
+//                 //$(".enemies").html("<img src='./assets/images/Gandalf.jpg' class='enemyFighter' value='110' alt='Gandalf'></img>"); 
+
+//                 // var newEnemy = $("<img>");
+//                 // newEnemy.addClass("enemyFighter");
+//                 // newEnemy.attr("src", enemySelect[i]);
+//                 // newEnemy.attr("value", enemyStats[i]);
+//                 // $(".enemies").append(newEnemy);
+//             }
+//         }
+// }
