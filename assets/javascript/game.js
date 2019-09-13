8ab2d7588e036abd37a79ad9f6e6a01b0099e9d3
Foster Gailey <foster@gaileyhome.com>
2019-09-13 15:15:39 -0400

    Attack button works for chosen fighter

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,27 +6,50 @@ var fighterSelected = {
     charAttack: Number,
     charName: "",
 };
-var defenderSelection = {
-    defHealth: Number,
-    defAttack: Number,
-    defName: "",
+var defenderSelected = {
+    charHealth: Number,
+    charAttack: Number,
+    charName: "",
 };
 
 
-var enemyStats = [80, 90, 70, 100]
 var fighterPicked = false;
 var defenderPicked = false;
+
+// var enemyStats = [80, 90, 70, 100]
 //this will start once the character has been chosen and display the enemy's
 //this doesnt work cause I cant click on one of these
 
 
 
+//when the defenders health drops below 0 
+//defender fades out and allows for a new defender to be selected
+//the stats should auto reset because of the defender on click function
+function defenderDefeat(){
+
+}
 
 $(document).ready(function() {
 
-    $(".attack").on("click", function(){
-         
+    $("#attack").on("click", function(){
+        if(defenderSelected.charHealth > 0){
+            //damage to defender
+            defenderSelected.charHealth -= fighterSelected.charAttack;
+            //doubles fighters damage
+            fighterSelected.charAttack += fighterSelected.charAttack;
+            //defender counter attacks
+            fighterSelected.charHealth -= defenderSelected.charAttack;
+
+            $("#charAttack").html(fighterSelected.charAttack);
+            $("#charHealth").html(fighterSelected.charHealth);
+            console.log(fighterSelected.charAttack);
+        
+            
+        }
+        
     });
+
+    
 
     //this creates a copy of the fighter in the fighting section with the value(hp) and fighting class.
     $(".fighters").on("click", function() {
@@ -43,29 +66,33 @@ $(document).ready(function() {
             fighter.attr("src", $(this).attr('src'));
             fighter.attr("value", $(this).attr('value'))
             $("#character").append(fighter);
+            //$(".character").fadeTo("fast", 0.2);
             $(this).parent().fadeOut();
 
             // adds name to the chosen fighter
             var fighterName = $("<div>");
             fighterName.addClass("charName");
             fighterName.text($(this).attr("data-name"));
+            fighterName.attr("id", "charName");
             $("#character").append(fighterName);
 
             //adds the attack attribute to the chosen fighter
             var fighterAttack = $("<div>");
             fighterAttack.addClass("charAttack");
             fighterAttack.text($(this).attr("data-attack"));
+            fighterAttack.attr("id", "charAttack");
             $("#character").append(fighterAttack);
 
             //adds the health attribute
             var fighterHealth = $("<div>");
             fighterHealth.addClass("charHealth");
             fighterHealth.text($(this).attr("data-health"));
+            fighterHealth.attr("id", "charHealth");
             $("#character").append(fighterHealth);
 
             //need to make an object for the character chosen
-            fighterSelected.charHealth = $(this).attr("data-health");
-            fighterSelected.charAttack = $(this).attr("data-attack");
+            fighterSelected.charHealth = parseInt($(this).attr("data-health"));
+            fighterSelected.charAttack = parseInt($(this).attr("data-attack"));
             fighterSelected.charName = $(this).attr("data-name")
             
             console.log(fighterSelected.charHealth);
@@ -84,7 +111,7 @@ $(document).ready(function() {
     $(".enemyFighter").on("click", function(){        
         //give a red glowing background in the class
         if (defenderPicked === false) {
-            console.log("working");
+            
             var defender = $("<img>");
             defender.addClass("defendingEnemy");
             defender.attr("src", $(this).attr('src'));
@@ -92,6 +119,12 @@ $(document).ready(function() {
             $("#defender").append(defender);
             $(this).fadeOut();
             
+            //assigns stats to the object
+            defenderSelected.charHealth = parseInt($(this).attr("data-health"));
+            defenderSelected.charAttack = parseInt($(this).attr("data-attack"));
+            defenderSelected.charName = $(this).attr("data-name");
+            console.log(defenderSelected.charName);
+
             defenderPicked = true;
         }
     });
